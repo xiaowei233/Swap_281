bd4c7fc1742e55e2f2d7d08324b91e88fb1a6b59
Allen Liu <all320@lehigh.edu>
2019-09-06 14:50:18 -0400

    setting up intial Vue and Spring instances

---
+++ Controller/swap281/src/test/java/com/swap281/swap281/Swap281ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.swap281.swap281;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Swap281ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
