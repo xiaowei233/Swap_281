13cb52d7e013a100a7dfb166b96d765ae72dbbdc
Yuesheng Xu <yux220@lehigh.edu>
2019-10-03 20:36:06 -0400

    change navbar 'test' to 'All Items'

--- View/app/src/components/Navbar.vue
+++ View/app/src/components/Navbar.vue
@@ -5,7 +5,7 @@
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item :to="{ name: 'item' }">TEST</b-nav-item>
+        <b-nav-item :to="{ name: 'item' }">All Items</b-nav-item>
         <b-nav-item :to="{ name: 'ItemPost' }">Post an Item</b-nav-item>
       </b-navbar-nav>
 
