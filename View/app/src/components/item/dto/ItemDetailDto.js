01c76a61921ac4c0ee64a6f0fcc3ea1ab4008997
Xiaowei Chen <45249152+xiaowei233@users.noreply.github.com>
2019-10-25 01:38:39 -0400

    Delete ItemDetailDto.js

--- View/app/src/components/item/dto/ItemDetailDto.js
+++
@@ -1,8 +0,0 @@
-export default class ItemDetail {
-    constructor(id, itemId, photo, detailDescription) {
-        this.id = id;
-        this.itemId = itemId;
-        this.photo = photo;
-        this.detailDescription = detailDescription;
-    }
-}
