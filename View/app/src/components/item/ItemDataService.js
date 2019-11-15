25c22fe125f33c2963866e1e190b5314a4bc0165
Lixuan Qiu <liq220@lehigh.edu>
2019-11-15 14:55:46 -0500

    Elasticsearch

--- View/app/src/components/item/ItemDataService.js
+++ View/app/src/components/item/ItemDataService.js
@@ -14,12 +14,25 @@ class ItemDataService {
         return axios.post(`${URL}/item/detail/post`, newItem);
     }
 
-    getItemCategoryFilter(){
+    getItemCategoryFilter() {
         return axios.get(`${URL}/item/detail/category-drop-down`);
     }
     // eslint-disable-next-line
     getItemById(itemId) {
         return axios.get(`${URL}/item/detail/${itemId}`);
+    }
+
+    // item/list/filter?category=1,2,3,4,5
+    getItemByCategoryIdList(idList) {
+        //console.log(idList)
+        if (idList.length !== 0 && idList !== undefined)
+            return axios.get(`${URL}/item/list/filter?category=${idList}`)
+        return axios.get(`${URL}/item/list/all`);
+    }
+
+    // eslint-disable-next-line
+    getCategoryList() {
+        return axios.get(`${URL}/item/list/dropdown`);
     }
 }
 
--- View/app/src/components/item/ItemList.vue
+++ View/app/src/components/item/ItemList.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="container">
+    <div class="cat">
+          <vs-select
+        placeholder="Select your categories"
+        multiple
+        label="Categories"
+        v-model="dropdown"
+      >
+        <vs-select-item
+          :key="index"
+          :value="item.id"
+          :text="item.category"
+          v-for="(item,index) in dropdown"
+          @input="executeLoader($event)"
+          @click="executeLoader($event)"
+        />
+      </vs-select>
+</div>
     <div class="row">
+
       <div class="col-lg-4 col-md-6 mb-4" v-for="item in items" v-bind:key="item.id">
         <div class="card h-100 customclass" @click="toItemDetail(item.id)">
           <a href="#">
@@ -23,18 +41,37 @@
 </template>
 
 <script>
+import Vue from 'vue'
+
+import { vsSelect } from 'vuesax'
+import 'vuesax/dist/vuesax.css'
+
 import ItemDataService from "./ItemDataService";
+Vue.use(vsSelect)
 
 export default {
   name: "ItemList",
   data() {
     return {
-      items: []
+      items: [],
+      dropdown: []
     };
   },
   methods: {
     refresh() {
       ItemDataService.getAllItems().then(res => {
+        this.items = res.data;
+      });
+      ItemDataService.getCategoryList().then(res => {
+        this.dropdown = res.data;
+      });
+    },
+    executeLoader(selectedItems) {
+      console.log(selectedItems);
+      let idList = [];
+      for (let i = 0; i < selectedItems.length; i++)
+        idList.push(selectedItems[i].id);
+      ItemDataService.getItemByCategoryIdList(idList).then(res => {
         this.items = res.data;
       });
     },
@@ -54,13 +91,15 @@ export default {
   cursor: pointer;
 }
 img {
-    position: relative;
-    margin-top: 10px;
-    width:  300px;
-    height: 300px;
-    background-position: 50% 50%;
-    background-repeat:   no-repeat;
-    background-size:     cover;
+  position: relative;
+  margin-top: 10px;
+  width: 300px;
+  height: 300px;
+  background-position: 50% 50%;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
-
+.cat {
+  margin: 20px;
+}
 </style>
